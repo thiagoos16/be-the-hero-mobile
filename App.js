28e2498476763697f225d3512ecc000f3ca00eb8
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-26 13:37:21 -0400

    add organization folders

--- App.js
+++ App.js
@@ -3,21 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Metal</Text>
+    <View>
+      <Text>Hello Metal</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E02041',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#fff'
-  }
-});
