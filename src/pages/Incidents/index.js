2268bf11fed967a5c2c65970c2360fbf1da7545f
thiagoos16 <tos@icomp.ufam.edu.br>
2020-03-26 15:10:46 -0400

    add initial incidents view

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
-export default function Incident() {
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles';
+
+export default function Incidents() {
   return (
-    <View />
+    <View styles={styles.container}> 
+        <View styles={styles.header}>
+            <Image source={logoImg} />
+            <Text style={styles.headerText}> 
+                Total de <Text style={styles.headerTextBold}> 30 casos </Text>
+            </Text>
+        </View>
+
+        <Text style={styles.title}> Welcome </Text>
+        <Text style={styles.description}> Escolha um dos casos abaixo e salve o dia!! </Text>
+    </View>
   );
 }
